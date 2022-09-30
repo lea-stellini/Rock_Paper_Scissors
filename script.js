ef0f74b6fa1d7e85d4d4bf551ff6c05ab35b681b
lea-stellini <lea.stellini@gmail.com>
2022-09-30 22:23:08 +0200

    started to create a fonction in script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const scissors = document.getElementById('scissors');
+
+const getRock = () =>{
+    console.log('rock');
+}
